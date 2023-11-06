98c6eba3649e4587c9e5186d767163793135e48d
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2023-11-06 21:51:05 +0700

    style: login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,6 +11,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
+    final buttonPadding = EdgeInsets.all(screenWidth * 0.03);
+    final buttonWidth = screenWidth * 0.42;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -23,70 +25,199 @@ class _LoginPageState extends State<LoginPage> {
         ),
         elevation: 0,
       ),
-      body: Padding(
-        padding: EdgeInsets.only(top: 25, left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Email',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            SizedBox(height: 8),
-            TextField(
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(screenWidth * 0.05),
-                hintText: "Email",
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                ),
-              ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Password',
-              style: TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            SizedBox(height: 8),
-            TextField(
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(screenWidth * 0.05),
-                hintText: "Password",
-                border: const OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                ),
-              ),
-            ),
-            SizedBox(height: 4),
-            Row(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 25, left: 20, right: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(0),
-                  child: Checkbox(
-                    value: false,
-                    onChanged: (value) {
-                      // Do something when the checkbox is checked or unchecked.
-                    },
-                    checkColor: Colors.white,
-                    fillColor: MaterialStateProperty.all(Colors.blue),
+                const Text(
+                  'Email',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black),
+                ),
+                const SizedBox(height: 8),
+                const TextField(
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    hintText: "Email",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6)),
+                      borderSide: BorderSide(color: Color(0xFFEAECF0)),
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFFF5FAFF),
                   ),
                 ),
-                Text(
-                  'Ingat Saya',
+                const SizedBox(height: 16),
+                const Text(
+                  'Kata Sandi',
+                  style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black),
                 ),
-                Text(
-                  'Lupa password?',
-                  style: TextStyle(color: Colors.blue),
+                const SizedBox(height: 8),
+                const TextField(
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                    hintText: "Kata Sandi",
+                    hintStyle: TextStyle(color: Colors.grey),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(6)),
+                      borderSide: BorderSide(color: Color(0xFFEAECF0)),
+                    ),
+                    filled: true,
+                    fillColor: Color(0xFFF5FAFF),
+                  ),
                 ),
+                const SizedBox(height: 4),
+                Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: (value) {
+                            // Do something when the checkbox is checked or unchecked.
+                          },
+                          checkColor: Colors.white,
+                          fillColor: MaterialStateProperty.all(Colors.blue),
+                        ),
+                        const Text(
+                          'Ingat Saya',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
+                    ),
+                    const Text(
+                      'Lupa password?',
+                      style: TextStyle(color: Color(0xFF194185), fontSize: 12),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Color(0xFF1570EF),
+                    ),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    minimumSize:
+                        MaterialStateProperty.all(Size(double.infinity, 48)),
+                  ),
+                  onPressed: () {},
+                  child: Text('Masuk'),
+                ),
+                const SizedBox(height: 28),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(color: Colors.grey),
+                      height: 1,
+                    ),
+                    const SizedBox(width: 10),
+                    const Text('atau masuk dengan'),
+                    const SizedBox(width: 10),
+                    Container(
+                      decoration: BoxDecoration(color: Colors.grey),
+                      height: 1,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 24),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: Colors.white,
+                        ),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset('images/logo_google.png'),
+                              SizedBox(width: 8),
+                              Text(
+                                "Google",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: const Color(0xFF3C5A9A),
+                        ),
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset('images/logo_fb.png'),
+                              SizedBox(width: 8),
+                              Text(
+                                "Facebook",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 36),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Belum mempunyai akun?',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300,
+                          color: Colors.black),
+                    ),
+                    SizedBox(width: 4),
+                    Text(
+                      'Daftar',
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Color(0xFF1570EF)),
+                    )
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
