2b00fc86baf0a754b06c521841771a072466dc6a
Muchammad Saifurrijaal <msaifurrijaal@gmail.com>
2023-11-08 08:56:56 +0700

    style : regis page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kampenies/pages/login_page.dart';
+import 'package:kampenies/pages/regis_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,9 +11,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+      routes: {
+        RegisterPage.routeName: (context) => RegisterPage(),
+      },
     );
   }
 }
